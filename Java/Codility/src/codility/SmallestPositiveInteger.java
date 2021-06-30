86e97d58b6b18966aa4822309d71f355c645aa73
Davessenn Louis-Jeune <ljoedavessenn@gmail.com>
2021-06-29 21:48:43 -0400

    Smallest positive integer

--- Java/Codility/src/codility/SmallestPositiveInteger.java
+++ Java/Codility/src/codility/SmallestPositiveInteger.java
@@ -13,44 +13,8 @@ import java.util.*;
  */
 public class SmallestPositiveInteger {
 
-    public static int solution(int[] A) {
-        // write your code in Java SE 8
-        Arrays.sort(A);
-        int n = A.length;
-        for (int i = 0; i < n; i++) {
-            if (A[i] > 0) {
-                if (i < n - 1) {
-                    if (A[i] != A[i + 1] && A[i] + 1 != A[i + 1]) {
-                        return A[i] + 1;
-                    }
-                } else {
-                    return A[n - 1] + 1;
-                }
-            }
-        }
-        return 1;
-    }
-
-    //performance ok
-    public static int solution2(int[] A) {
-        // write your code in Java SE 8
-        Arrays.sort(A);
-        int n = A.length;
-        for (int i = 0; i < n - 1; i++) {
-            if (A[i] >= 0 && A[i] < Integer.MAX_VALUE && A[i] + 1 < A[i + 1]) {
-                return A[i] + 1;
-            }
-        }
-        if (A[n - 1] < Integer.MAX_VALUE) {
-
-            return A[n - 1] < 0 ? 1 : A[n - 1] + 1;
-        } else {
-            return 1;
-        }
-    }
-
     //correctness ok
-    public static int solution3(int[] A) {
+    public static int solution2(int[] A) {
     Set<Integer> testedSet = new TreeSet();
     Set<Integer> perfectSet = new TreeSet();
    
@@ -72,14 +36,38 @@ public class SmallestPositiveInteger {
     return 1; //default - e.g. if A array has negative values or just a single positive value like 10
   }
     
+    //The good solution
+    public static int solution(int[] A) {
+        Arrays.sort(A);
+        int i = 0;
+        int n = A.length;
+        int response = 1;
+        if (n == 0) {
+            return response;
+        } else {
+            while (i < n && A[i] < 1) {
+                i++;                
+                System.out.println("i "+i);
+            }
+            if(i==n || A[i]>1){
+              return response;  
+            } else {
+                while(i<n-1 && A[i+1]<=A[i]+1){
+                    i++;
+                }
+                return A[i]+1;
+            }
+        }
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int a[] = {Integer.MAX_VALUE};
+        int a[] = {};
 //        a[0]=1;
-        System.out.println("sol " + solution2(a));
+        System.out.println("sol " + solution(a));
     }
 
 }
